ef814d0d6eb2d6c25ad5b7314bdd51acfad5e90c
Franklyn Medeiros <franklynkleiton18@gmail.com>
2021-01-13 19:53:49 -0300

    Registering user.

--- micro-user/api.js
+++ micro-user/api.js
@@ -1,18 +1,36 @@
+const bcrypt = require('bcrypt');
 
 class Api {
   mongo = null;
   grpc = null;
+  messages = null;
   
   constructor(db, grpc) {
     this.mongo = db;
     this.grpc = grpc;
   }
 
+  addMessages(messages) {
+    messages = messages;
+  }
+
   register(call, callback) {
-    callback(null, {
-      id: 1,
-      email: 'test',
-      name: 'test',
+    const users = this.db.collection('users');
+
+    bcrypt.hash(call.request.getPassword(), 10, (err, hash) => {
+      let user = { name: call.request.getName(), email: call.request.getEmail(), password: hash };
+      users
+        .insertOne(user)
+        .then(() => {
+          console.log(this.messages);
+          let response = new this.messages.User();
+          response.setId(user._id.toString());
+          response.setName(user.name);
+          response.setEmail(user.email);
+          response.setName(user.name);
+          response.setToken(auth.generateToken(user));
+          callback(null, response);
+        });
     });
   }
 
